7dfce8cdbfef84cb0ece5438fc2353b4eb336117
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2024-02-12 16:51:51 -0300

    removing unnecessary routes

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,24 +1,18 @@
 import { Router } from "core/router";
-import { upload } from "core/modules";
 
 const useRouter = Router();
 
+/**
+ * Direct routes
+ * Used, mainly, when you want to make static views
+ */
 useRouter.get("/", (req, res) => res.render("home"));
 useRouter.get("/404", (req, res) => res.render("404"));
 
-useRouter.get("/count", (req, res) => {
-  res.render("counter", { title: "Inicio", count: 0 });
-});
-
-useRouter.post("/upload", upload.single("file"), (req, res) => {
-  return res.json(req.file?.filename);
-});
-
+/**
+ * Resources
+ * Used when you want to make a default CRUD
+ */
 useRouter.resources("users", "UsersController");
 
 export default useRouter;
-
-/**
- * Resources
- */
-// useRouter.resources("users", "UsersController");
