4f561bf3cfe9b6be18984d87af98a04def845d42
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2024-02-12 16:46:57 -0300

    removing unnecessary files

--- app/resources/js/counter.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("increment").addEventListener("click", () => {
-  const countDisplay = document.getElementById("count-display");
-  const currentCount = parseInt(countDisplay.textContent, 10);
-  countDisplay.textContent = currentCount + 1;
-});
