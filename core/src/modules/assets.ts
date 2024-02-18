627fd28532a5cfd6197ca2a43ba20fbeace404ec
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2024-02-18 03:13:13 -0300

    add assets and images static directories

--- core/src/modules/assets.ts
+++ core/src/modules/assets.ts
@@ -6,6 +6,8 @@ export default function Assets(APP: Express, ROOT_PATH: string) {
   APP.use(express.static(join(ROOT_PATH, "public")));
   APP.use("/css", express.static(join(ROOT_PATH, isProd, "public/css")));
   APP.use("/js", express.static(join(ROOT_PATH, isProd, "app/resources/js")));
+  APP.use("/images", express.static(join(ROOT_PATH, isProd, "public/images")));
+  APP.use("/assets", express.static(join(ROOT_PATH, isProd, "public/assets")));
 
   if (process.env.DRIVE === "local") {
     APP.use("/files", express.static(join(ROOT_PATH, "uploads")));
--- core/src/modules/views.ts
+++ core/src/modules/views.ts
@@ -4,10 +4,10 @@ import nunjucks from "nunjucks";
 import { isProd } from "../global";
 
 export default function Views(APP: Express, ROOT_PATH: string) {
-  const viewsPath = join(ROOT_PATH, isProd, "app", "resources", "views");
-  const layoutsPath = join(ROOT_PATH, isProd, "app", "resources", "layouts");
-  const partialsPath = join(ROOT_PATH, isProd, "app", "resources", "partials");
-  const mailsPath = join(ROOT_PATH, isProd, "app", "resources", "mails");
+  const viewsPath = join(ROOT_PATH, isProd, "app/resources/views");
+  const layoutsPath = join(ROOT_PATH, isProd, "app/resources/layouts");
+  const partialsPath = join(ROOT_PATH, isProd, "app/resources/partials");
+  const mailsPath = join(ROOT_PATH, isProd, "app/resources/mails");
 
   APP.engine("nj", nunjucks.render);
   APP.set("view engine", "nj");
