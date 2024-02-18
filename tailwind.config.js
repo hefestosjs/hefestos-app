f49117a880bd7bbc9926a35bb4eedc2b901b0f1c
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2024-02-18 03:12:07 -0300

    remove daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    require("daisyui"),
-  ],
+  plugins: [require("tailwindcss"), require("autoprefixer")],
 };
