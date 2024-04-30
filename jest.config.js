2465659588abda5489c4263496f0ae6feb3ef06f
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2024-04-30 11:10:12 -0300

    chore: add fakerjs library, remove @path and rewrite the jest config file

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  collectCoverage: true,
-  coverageReporters: ["json", "html"],
+  verbose: true,
+
+  rootDir: ".",
+  testPathIgnorePatterns: ["/node_modules/"],
+
+  collectCoverageFrom: [
+    "app/controllers/**/*.ts",
+    "app/services/**/*.ts",
+    "app/tasks/**/*.ts",
+  ],
+  coverageReporters: ["json", "html", "text"],
+
+  modulePaths: ["<rootDir>"],
+  moduleDirectories: ["node_modules", "app"],
+  moduleFileExtensions: ["js", "ts"],
 };
