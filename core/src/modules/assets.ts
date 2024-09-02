9df8ce2f9bdc132516351107c82f022754298219
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2024-09-02 14:54:15 -0300

    fix: update drive env variable to drive_disk in assets modules

--- core/src/modules/assets.ts
+++ core/src/modules/assets.ts
@@ -3,15 +3,15 @@ import express, { type Express } from "express";
 import { isProd } from "../global";
 
 export default function Assets(APP: Express) {
-	const ROOT_PATH = process.cwd();
+  const ROOT_PATH = process.cwd();
 
-	APP.use(express.static(join(ROOT_PATH, "public")));
-	APP.use("/css", express.static(join(ROOT_PATH, isProd, "public/css")));
-	APP.use("/js", express.static(join(ROOT_PATH, isProd, "app/resources/js")));
-	APP.use("/images", express.static(join(ROOT_PATH, isProd, "public/images")));
-	APP.use("/assets", express.static(join(ROOT_PATH, isProd, "public/assets")));
+  APP.use(express.static(join(ROOT_PATH, "public")));
+  APP.use("/css", express.static(join(ROOT_PATH, isProd, "public/css")));
+  APP.use("/js", express.static(join(ROOT_PATH, isProd, "app/resources/js")));
+  APP.use("/images", express.static(join(ROOT_PATH, isProd, "public/images")));
+  APP.use("/assets", express.static(join(ROOT_PATH, isProd, "public/assets")));
 
-	if (process.env.DRIVE === "local") {
-		APP.use("/files", express.static(join(ROOT_PATH, "uploads")));
-	}
+  if (process.env.DRIVE_DISK === "local") {
+    APP.use("/files", express.static(join(ROOT_PATH, "uploads")));
+  }
 }
